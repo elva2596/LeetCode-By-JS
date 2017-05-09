7b4f13ac11a0c815de0749efee0fc1ad7dd1cda4
elva2596 <646994014@qq.com>
2017-05-09 23:34:32 +0800

    delete LinkList

--- LinkList/141-Linked List Cycle.js
+++ LinkList/141-Linked List Cycle.js
@@ -1,3 +1,72 @@
 /**
- * Created by 路路 on 2017/5/8.
+ * Given a linked list, determine if it has a cycle in it.
+ * 判断单链表是否有环
  */
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/*
+   判断单链表是否有环
+     使用快慢指针的方法:
+        1.用两个指针fast和slow分别指向头结点，即fast = slow = head
+        2.fast和slow分别采用1和2作为步长遍历该链表(fast应该检查其下一个结点是否为空)
+        3.如果fast遇到了null,则证明该链表没有环；若fast和slow在某时刻指向同一结点，则说明该链表有环
+   【判断环的入口】:
+         固定fast在他们第一次相遇的结点，然后slow回到表头，然后它们均以1为步长遍历链表，则他们再次相遇时，
+         即为环开始结点
+ */
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function(head) {
+    var fast = head;
+    var slow = head;
+    while(fast&&fast.next){
+        fast = fast.next.next;
+        slow = slow.next;
+        if(fast==slow){
+            return true
+        }
+    }
+    return false;
+};
+
+
+/*
+    先判断单链表是否有环，如果有环找到环的入口
+ */
+ /**
+ * @param {ListNode} head
+ * @return {ListNode} loopPort
+ */
+
+ var findLoopPort = function (head){
+     var fast = head;
+     var slow = head;
+     while(fast&&fast.next){
+         fast = fast.next.next;
+         slow = slow.next;
+         //存在环的情况下终止循环
+         if(fast==slow){
+             break;
+         }
+     }
+     //不存在环(即循环终止也没有找到环)
+     if(fast!=slow){
+         return null
+     }
+
+     slow = head;
+     while(fast!=slow){
+         slow =slow.next;
+         fast= fast.next;
+     }
+     return fast;
+
+ };
--- LinkList/206-Reverse Linked List.js
+++ LinkList/206-Reverse Linked List.js
@@ -1,3 +1,98 @@
 /**
- * Created by 路路 on 2017/5/8.
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
  */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head) {
+    /*
+
+     iteratively
+
+     方法一:使用三个变量
+     使用p和q两个指针配合工作，使得两个结点间的指向反向，同时用记录剩下的链表
+
+     var p = head,
+     q = head.next,
+     r;
+     if(head===null||head.next===null){
+     return head;
+     }
+     head.next = null;
+     while(q){
+     r =q.next;
+     q.next = p;
+     p=q;
+     q=r;
+     }
+     return p
+
+     or 使用一个中间变量
+
+     var cur  =head,
+     pre = null;
+     if(head===null||head.next===null){
+     return head;
+     }
+
+     while(cur){
+     var temp = cur.next;
+     cur.next = pre;
+     pre = cur;
+     cur = temp
+     }
+     return pre
+     */
+
+
+    /*
+     iteratively
+
+     方法二:使用两个变量;
+     从单链表的第二个元素开始一直到链表的末尾，将元素依次插入到第一个结点(head)之后，最后将第一个链表挪到新链表的最后
+
+     var p = head.next,
+     q;
+     if(head===null||head.next===null){
+     return head
+     }
+     while(p.next){
+     q= p.next;
+     p.next = q.next;
+     q.next = head.next;
+     head.next = q;
+     }
+
+     //将旧的head移到新链表的末尾
+     p.next = head;//形成环，将旧的head移到新的链表的最后一个元素以后
+     head = p.next.next;//将新head移到第一个元素上
+     p.next.next = null;//断掉旧head与第一个元素的连接指针
+     return head;
+     */
+
+
+    /*
+
+     递归实现:
+     从后往前反转，从最后两个结点开始反转，依次向前，将后一个链表结点指向前一个结点，注意每次反转后要将原链表中前一个
+     结点的指针域置空，表示将原链表中前一个结点指向后一个结点的指向关系断开
+
+
+     if(head===null||head.next===null){
+     return head
+     }else{
+     var newNode = reverseList(head.next);
+     console.log(newNode)
+     head.next.next= head;
+     head.next = null
+     return newNode
+     }
+     */
+
+
+}
